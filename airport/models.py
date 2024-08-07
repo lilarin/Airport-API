858441a5cdbede921dd1eae9d4e9b3cef67297e8
lilarin <80650911+lilarin@users.noreply.github.com>
2024-08-07 18:28:34 +0300

    Addition of the validation to the tickets

--- airport/models.py
+++ airport/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -71,6 +72,9 @@ class Order(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="orders"
     )
 
+    def __str__(self):
+        return f"{self.user} ({self.created_at})"
+
 
 class Ticket(models.Model):
     row = models.IntegerField()
@@ -81,3 +85,31 @@ class Ticket(models.Model):
     order = models.ForeignKey(
         Order, on_delete=models.CASCADE, related_name="tickets"
     )
+
+    @staticmethod
+    def validate_ticket(row, seat, cinema_hall, error_to_raise):
+        for ticket_attr_value, ticket_attr_name, flight_airplane_attr_name in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row"),
+        ]:
+            count_attrs = getattr(cinema_hall, flight_airplane_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} "
+                                          f"number must be in available range: "
+                                          f"(1, {flight_airplane_attr_name}): "
+                                          f"(1, {count_attrs})"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.flight.airplane,
+            ValidationError,
+        )
+
+    def __str__(self):
+        return f"{self.order} (Flight: {self.flight})"
