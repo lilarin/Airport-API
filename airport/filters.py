c851d0a3df8e11e2c848c81ad81a88bba72a81ed
lilarin <80650911+lilarin@users.noreply.github.com>
2024-08-08 22:37:58 +0300

    Implementation of the new filters

--- airport/filters.py
+++ airport/filters.py
@@ -10,25 +10,87 @@ class NameFilter(FilterSet):
 
 class AirportFilter(FilterSet):
     name = filters.CharFilter(
-        field_name='name', lookup_expr='icontains'
+        field_name="name", lookup_expr="icontains"
     )
     city_name = filters.CharFilter(
-        field_name='city__name', lookup_expr='icontains'
+        field_name="city__name", lookup_expr="icontains"
     )
     country_name = filters.CharFilter(
-        field_name='country__name', lookup_expr='icontains'
+        field_name="country__name", lookup_expr="icontains"
     )
 
 
 class FlightFilter(FilterSet):
+    country = filters.CharFilter(
+        field_name="route__destination__country__name",
+        lookup_expr="icontains"
+    )
+    city = filters.CharFilter(
+        field_name="route__destination__city__name",
+        lookup_expr="icontains"
+    )
+    departure_time = filters.DateFilter(
+        field_name="departure_time", lookup_expr="date"
+    )
+    arrival_time = filters.DateFilter(
+        field_name="arrival_time", lookup_expr="date"
+    )
+
+
+class TicketFilter(FilterSet):
+    country = filters.CharFilter(
+        field_name="flight__route__destination__country__name",
+        lookup_expr="icontains"
+    )
+    city = filters.CharFilter(
+        field_name="flight__route__destination__city__name",
+        lookup_expr="icontains"
+    )
+    departure_time = filters.DateFilter(
+        field_name="flight__departure_time", lookup_expr="date"
+    )
+    arrival_time = filters.DateFilter(
+        field_name="flight__arrival_time", lookup_expr="date"
+    )
     name = filters.CharFilter(
-        field_name='name', lookup_expr='icontains'
-    )
-    city_name = filters.CharFilter(
-        field_name='city__name', lookup_expr='icontains'
-    )
-    country_name = filters.CharFilter(
-        field_name='country__name', lookup_expr='icontains'
+        field_name="flight__airplane__name", lookup_expr="icontains"
     )
 
 
+class OrderFilter(FilterSet):
+    country = filters.CharFilter(
+        field_name="tickets__flight__route__destination__country__name",
+        lookup_expr="icontains"
+    )
+    city = filters.CharFilter(
+        field_name="tickets__flight__route__destination__city__name",
+        lookup_expr="icontains"
+    )
+    departure_time = filters.DateFilter(
+        field_name="tickets__flight__departure_time", lookup_expr="date"
+    )
+    arrival_time = filters.DateFilter(
+        field_name="tickets__flight__arrival_time", lookup_expr="date"
+    )
+    name = filters.CharFilter(
+        field_name="tickets__flight__airplane__name", lookup_expr="icontains"
+    )
+
+
+class RouteFilter(FilterSet):
+    source_country = filters.CharFilter(
+        field_name="source__country__name",
+        lookup_expr="icontains"
+    )
+    source_city = filters.CharFilter(
+        field_name="source__city__name",
+        lookup_expr="icontains"
+    )
+    destination_country = filters.CharFilter(
+        field_name="destination__country__name",
+        lookup_expr="icontains"
+    )
+    destination_city = filters.CharFilter(
+        field_name="destination__city__name",
+        lookup_expr="icontains"
+    )
