ed769a2dd94347f512fbf347adf322841984c308
lilarin <80650911+lilarin@users.noreply.github.com>
2024-08-09 15:18:36 +0300

    Added documentation routes to the project routes

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -17,9 +17,16 @@ Including another URLconf
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/airport/", include("airport.urls", namespace="airport")),
     path("api/user/", include("user.urls", namespace="user")),
+    path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui"
+    ),
 ] + debug_toolbar_urls()
